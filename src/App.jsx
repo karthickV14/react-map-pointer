a0f115c5aa4a2148fc833335af0282376f107a1e
karthickV14 <karthickvinayagamoorthi@gmail.com>
2024-12-17 15:07:25 +0530

    Fack Authentication login logout implemented

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
@@ -21,7 +23,14 @@ const App = () => {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
@@ -32,7 +41,7 @@ const App = () => {
           </Routes>
         </BrowserRouter>
       </CitiesProvider>
-    </>
+    </AuthProvider>
   );
 };
 
